66fd451253bbc612a02989c159cd979f04517bb1
root <root@adriatic.ddpsc.org>
2018-04-21 18:07:17 -0500

    Started writing DML code for inserting data

---
+++ dml/insertdata.py
@@ -0,0 +1,59 @@
+from configparser import ConfigParser
+import psycopg2
+import csv
+
+def config(filename='database.ini', section='postgresql'):
+  # create a parser
+  parser = ConfigParser()
+  # read config file
+  parser.read(filename)
+
+  # get section, default to postgresql
+  db = {}
+  if parser.has_section(section):
+    params = parser.items(section)
+    for param in params:
+      db[param[0]] = param[1]
+  else:
+    raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+  return db
+
+def connect():
+  conn = None
+  try:
+    # read connection parameters
+    params = config()
+
+    # connect to the PostgreSQL server
+    print('Connection to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
+
+    # create a cursor
+    cur = conn.cursor()
+
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    db_version = cur.fetchone()
+    print(db_version)
+
+    # Close the connection
+    cur.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+  
+  return conn
+
+def insert_species(conn, species):
+  cur = conn.cursor()
+  SQL = """INSERT INTO species (shortname, binomial, subspecies, variety)
+        VALUES (%s, %s, %s, %s);"""
+  args_tuple = (species.n, species.b, species.s, species.v)
+  cur.execute(SQL, args_tuple)
+  newID = cursor.fetchone()[0]
+  conn.commit()
+  cur.close()
+  return newID
+
+
