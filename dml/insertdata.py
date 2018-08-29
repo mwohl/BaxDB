dcd6aa93dbd5e7e896e3d9d3a9231ec326bbdb23
Molly Wohl <mollywohl@OSX-ToppLab11.local>
2018-08-29 15:58:44 -0700

    separated connect() and config() out into dbconnect.py

--- dml/insertdata.py
+++ dml/insertdata.py
@@ -1,53 +1,10 @@
-from configparser import ConfigParser
 import pandas as pd
 import numpy as np
 import psycopg2
 import csv
 import insert
+from dbconnect import config, connect
 from models import species, population, line, chromosome, variant, genotype, trait, phenotype, growout_type, growout, location, gwas_algorithm, genotype_version, imputation_method, kinship_algorithm, kinship, population_structure_algorithm, population_structure, gwas_run, gwas_result
-
-def config(filename='database.ini', section='postgresql'):
-  # create a parser
-  parser = ConfigParser()
-  # read config file
-  parser.read(filename)
-
-  # get section, default to postgresql
-  db = {}
-  if parser.has_section(section):
-    params = parser.items(section)
-    for param in params:
-      db[param[0]] = param[1]
-  else:
-    raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-  return db
-
-def connect():
-  conn = None
-  try:
-    # read connection parameters
-    params = config()
-
-    # connect to the PostgreSQL server
-    print('Connection to the PostgreSQL database...')
-    conn = psycopg2.connect(**params)
-
-    # create a cursor
-    cur = conn.cursor()
-
-    print('PostgreSQL database version:')
-    cur.execute('SELECT version()')
-
-    db_version = cur.fetchone()
-    print(db_version)
-
-    # Close the connection
-    cur.close()
-  except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
-  
-  return conn
 
 def find_species(conn, speciesShortname):
   cur = conn.cursor()
